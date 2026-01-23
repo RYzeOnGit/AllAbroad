c66c45bbf8cf4ac55baf24d7136ae3e3cb562521
Ryan <ryan.chattopadhyay1@gmail.com>
2026-01-23 15:51:25 -0700

    ports

--- app/main.py
+++ app/main.py
@@ -49,10 +49,11 @@ app.add_middleware(RequestLoggingMiddleware)
 # CORS middleware for frontend integration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # Include API routes
